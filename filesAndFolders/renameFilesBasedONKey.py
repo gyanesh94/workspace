4268e71f5bc4199ee12347170dc6629061cccfe1
gyanesh94 <gyanesh.cse@gmail.com>
2016-06-19 22:27:41 +0530

    removed url file processing and some condition checking added

--- filesAndFolders/renameFilesBasedONKey.py
+++ filesAndFolders/renameFilesBasedONKey.py
@@ -2,30 +2,20 @@
 
 import os
 from renameFilesBasedONKeyConfig import *
-from urlparse import urlparse
 import logging
 
 logging.basicConfig(filename=LOGGING_FILE_NAME, format=LOGGING_MESSAGE_FORMAT, datefmt=LOGGING_DATE_FORMAT, level=logging.DEBUG)
 nameMapping = {}
 
 
-def removeDuplicatesFromUrlTxt(key):
-    text = []
-    with open(URL_FILE_PATH, 'r') as urlData:
-        for url in urlData.readlines():
-            a = urlparse(url)
-            if key == a.path.split("/")[-1].replace(".mp4", ""):
-                continue
-            text.append(url)
-
-    with open(URL_FILE_PATH, 'w') as file:
-        file.writelines(text)
-
-
 def getNameMapping():
+    if not os.path.exists(NAME_FILE_PATH):
+        logging.error("{file} doesn't exist".format(file=NAME_FILE_PATH))
+        return False
+
     with open(NAME_FILE_PATH, 'r') as nameData:
         for line in nameData.readlines():
-            if line.strip == "":
+            if line.strip() == "":
                 continue
             lineArray = line.split(SPLIT_CONDITION, 1)
             if len(lineArray) < 2:
@@ -34,17 +24,20 @@ def getNameMapping():
             name = lineArray[1].strip()
             name = name.replace("/", " ").replace("#", " ").replace("$", " ").replace("\\", " ").replace("'", " ").replace('"', " ").replace("\n", " ")
             if key in nameMapping:
-                logging.warn("{key} is duplicate".format(key=key))
-                removeDuplicatesFromUrlTxt(key)
+                logging.warn("{key} is duplicate while creating nameMapping".format(key=key))
             else:
                 if ADD_KEY_TO_NAME:
                     nameMapping[key] = "{key} {name}{extension}".format(key=key, name=name, extension=EXTENSION)
                 else:
                     nameMapping[key] = "{name}{extension}".format(name=name, extension=EXTENSION)
 
+    return True
+
 
 def makeUniqueNameList():
-    getNameMapping()
+    flag = getNameMapping()
+    if not flag:
+        exit()
     text = ""
     for key, name in nameMapping.iteritems():
         text = text + name.replace(EXTENSION, "") + "\n"
@@ -53,11 +46,15 @@ def makeUniqueNameList():
 
 
 def renameFiles():
-    getNameMapping()
+    flag = getNameMapping()
+    if not flag:
+        exit()
     for i in os.listdir(BASE_PATH):
         filePath = BASE_PATH + i
         baseName, extension = os.path.splitext(i)
         if baseName in nameMapping and os.path.isfile(filePath) and extension == EXTENSION:
             os.rename(filePath, BASE_PATH + nameMapping[baseName])
+        else:
+            logging.error("mapping id not present for {key}".format(key=i))
 
 renameFiles()
