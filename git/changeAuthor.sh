097f42d1e21f07d7525ca0b4723537110179d9fd
gyanesh94 <gyanesh.cse@gmail.com>
2016-07-24 18:32:23 +0530

    git changeAuthor

---
+++ git/changeAuthor.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Change old author to new author
+
+
+git filter-branch --env-filter '
+OLD_EMAIL="oldemail@gmail.com"
+CORRECT_NAME="newname"
+CORRECT_EMAIL="newemail@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
+git push --force --tags origin 'refs/heads/*'
